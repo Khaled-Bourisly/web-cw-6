0f1db6c03e4181ae54709d215dce3506eafb3423
Khaled-Bourisly <bourislyk00@gmail.com>
2020-07-02 21:14:05 +0300

    Update scripts.js

--- Development/js/scripts.js
+++ Development/js/scripts.js
@@ -51,5 +51,20 @@ getBookById(562 , readbooks)
 
 // التمرين الثاني
 
+let Items = [];
+
+let name = prompt("Product Name");
+
+while (name !="Done") {
+    let price = prompt("Enter Price ");
+    let qunatity = prompt("Quantity");
+
+    items.push({name2: name, price2: price, qunatity2: qunatity});
+
+  name = prompt("Product Name");
+}
+for(l of items ){
+    console.log(l.name2+" "+l.price2+" "+l.qunatity2);
+}
 
 
